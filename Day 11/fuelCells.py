3e3b88a7a7512023d1a2e8048e3ccb2176ebaae3
Carls13 <charlesshb98@gmail.com>
2018-12-13 14:06:51 -0400

    Added Day 11 part 1

---
+++ Day 11/fuelCells.py
@@ -0,0 +1,56 @@
+#Part 1
+
+myGridSerial = 8561
+fuelCells = []
+
+# Calculate each cell's power level
+for y in range(300):
+    yCoord = y+1
+    actualYCells = []
+    for x in range(300):
+        xCoord = x + 1
+        xyPoint ='('+ str(xCoord) + ','+str(yCoord)+')'
+        rackId = xCoord + 10
+        powerLevel = rackId * yCoord
+        powerLevel += myGridSerial
+        powerLevel = powerLevel * rackId
+        powerLevel = (powerLevel%1000)//100
+        powerLevel -= 5
+        actualYCells.append([xyPoint, powerLevel])
+    fuelCells.append(actualYCells)
+    
+mainCellPower = []
+
+# For each 3x3 area we will define it like: 
+# abc
+# def
+# ghi
+for y in range(298):
+    for x in range(298):
+        areaPower = 0
+        # Gets top left cell 
+        topLeft = fuelCells[y][x][0]
+        # Gets each cell's power level
+        a = fuelCells[y][x][1]
+        b = fuelCells[y+1][x][1]
+        c = fuelCells[y+2][x][1]
+        d = fuelCells[y][x+1][1]
+        e = fuelCells[y+1][x+1][1]
+        f = fuelCells[y+2][x+1][1]
+        g = fuelCells[y][x+2][1]
+        h = fuelCells[y+1][x+2][1]
+        i = fuelCells[y+2][x+2][1]
+        # Sums all of them
+        areaPower = a + b + c + d + e + f + g + h + i
+        mainCellPower.append([topLeft, areaPower])
+
+# Compares greatest
+greatestPower = -1000
+greatestCoord = ''
+
+for area in mainCellPower:
+    if area[1] > greatestPower:
+        greatestPower = area[1]
+        greatestCoord = area[0]
+
+print(greatestCoord)
