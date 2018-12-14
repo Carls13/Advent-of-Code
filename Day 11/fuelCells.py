d903c3bed2ab22adde92067a7ccf0bae91baee81
Carls13 <charlesshb98@gmail.com>
2018-12-13 23:03:46 -0400

    Progress part 2

--- Day 11/fuelCells.py
+++ Day 11/fuelCells.py
@@ -19,38 +19,51 @@ for y in range(300):
         actualYCells.append([xyPoint, powerLevel])
     fuelCells.append(actualYCells)
     
-mainCellPower = []
+# mainCellPower = []
 
-# For each 3x3 area we will define it like: 
-# abc
-# def
-# ghi
-for y in range(298):
-    for x in range(298):
-        areaPower = 0
-        # Gets top left cell 
-        topLeft = fuelCells[y][x][0]
-        # Gets each cell's power level
-        a = fuelCells[y][x][1]
-        b = fuelCells[y+1][x][1]
-        c = fuelCells[y+2][x][1]
-        d = fuelCells[y][x+1][1]
-        e = fuelCells[y+1][x+1][1]
-        f = fuelCells[y+2][x+1][1]
-        g = fuelCells[y][x+2][1]
-        h = fuelCells[y+1][x+2][1]
-        i = fuelCells[y+2][x+2][1]
-        # Sums all of them
-        areaPower = a + b + c + d + e + f + g + h + i
-        mainCellPower.append([topLeft, areaPower])
+# # For each 3x3 area we will define it like: 
+# # abc
+# # def
+# # ghi
+# for y in range(298):
+#     for x in range(298):
+#         areaPower = 0
+#         # Gets top left cell 
+#         topLeft = fuelCells[y][x][0]
+#         # Gets each cell's power level
+#         a = fuelCells[y][x][1]
+#         b = fuelCells[y+1][x][1]
+#         c = fuelCells[y+2][x][1]
+#         d = fuelCells[y][x+1][1]
+#         e = fuelCells[y+1][x+1][1]
+#         f = fuelCells[y+2][x+1][1]
+#         g = fuelCells[y][x+2][1]
+#         h = fuelCells[y+1][x+2][1]
+#         i = fuelCells[y+2][x+2][1]
+#         # Sums all of them
+#         areaPower = a + b + c + d + e + f + g + h + i
+#         mainCellPower.append([topLeft, areaPower])
 
-# Compares greatest
-greatestPower = -1000
-greatestCoord = ''
+# # Compares greatest
+# greatestPower = -1000
+# greatestCoord = ''
 
-for area in mainCellPower:
-    if area[1] > greatestPower:
-        greatestPower = area[1]
-        greatestCoord = area[0]
+# for area in mainCellPower:
+#     if area[1] > greatestPower:
+#         greatestPower = area[1]
+#         greatestCoord = area[0]
 
-print(greatestCoord)
+# print(greatestCoord)
+
+# Part 2
+# Using previous data
+size = 1
+while size < 31:
+    for y in range(300 - (size - 1) ):
+        for x in range(300 - (size - 1) ):
+            areaPower = 0
+            yi = y
+            xi = x
+            while yi <(yi + size - 1):
+                while xi < (xi + size - 1):
+                    areaPower += fuelCells[yi][xi]
